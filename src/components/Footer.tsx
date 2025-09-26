528b70f54ef6870805287fa7b404399b4d74fe49
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2025-09-26 09:38:54 +0545

    change footer date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2020 <a href="/" className="hover:underline">Faux Magazine™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">Faux Magazine™</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
